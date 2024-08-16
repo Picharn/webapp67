8de69a64894af5ff8c41901420cd368d9183f1f0
Picharn <30793191+Picharn@users.noreply.github.com>
2024-08-16 10:47:12 +0700

    Add files via upload

---
+++ myweb/receivelogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="mystyle.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> ยินดีต้อนรับ </h1>
+    
+    
+        <?php
+            $showuser = $_POST['user'];
+            echo "Welcome to cs supermarket websit: $showuser";
+        ?> 
+   <form action="shopping.html">
+    shopping click here! <input type="submit" name="shop" value="shopping here!">
+</form>
+
+</body>
+</html>
+
+
+
